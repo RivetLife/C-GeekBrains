a078e319f660e019f552ac22dbc4e5fa59b115f2
RivetLife <rivet.life@mail.ru>
2018-11-17 04:32:08 +0300

    Домашнее задание на четвертый урок.

---
+++ Lesson4/Lesson4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Илья Бугров
+//Меню
+
+namespace Lesson4
+{
+    partial class Program
+    {
+        static void Menu()
+        {
+            Console.Write("Введите номер задания: ");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    Task1();
+                    break;
+                case "2":
+                    Task2();
+                    break;
+                case "3":
+                    Task3();
+                    break;
+                default:
+                    Console.WriteLine("Введен неверный номер задания.");
+                    break;
+            }
+        }
+
+        static void Main()
+        {
+            Menu();
+        }
+    }
+}
---
+++ Lesson4/Lesson4/Task3.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Илья Бугров
+//а) Дописать класс для работы с одномерным массивом.
+//Реализовать конструктор, создающий массив определенного размера и
+//заполняющий массив числами от начального значения с заданным шагом.
+//Создать свойство Sum, которое возвращает сумму элементов массива, метод Inverse,
+//возвращающий новый массив с измененными знаками у всех элементов массива
+//(старый массив, остается без изменений),  метод Multi,
+//умножающий каждый элемент массива на определённое число, свойство MaxCount,
+//возвращающее количество максимальных элементов. 
+
+namespace Lesson4
+{
+    class Massive
+    {
+        int[] mass;
+
+        public Massive(int size, int start, int step)
+        {
+            mass = new int[size];
+            mass[0] = start;
+            for (int i = 1; i < mass.Length; i++)
+            {
+                mass[i] = mass[i - 1] + step;
+            }
+        }
+
+        public int Sum
+        {
+            get
+            {
+                int sum = 0;
+                foreach (int el in mass)
+                {
+                    sum += el;
+                }
+                return sum;
+            }
+        }
+
+        public int[] Inverse()
+        {
+            int[] inv = new int[mass.Length - 1];
+            for (int i = 0; i < mass.Length; i++)
+            {
+                inv[i] = mass[i] * -1;
+            }
+            return inv;
+        }
+
+        public void Multi(int multiplier)
+        {
+            for (int i = 0; i < mass.Length; i++)
+            {
+                mass[i] *= multiplier;
+            }
+        }
+
+        public int MaxCount
+        {
+            get
+            {
+                int max = mass[0];
+                int maxCount = 0;
+                foreach(int el in mass)
+                {
+                    if (el > max)
+                    {
+                        max = el;
+                    }
+                }
+                foreach(int el in mass)
+                {
+                    if (el == max)
+                    {
+                        maxCount++;
+                    }
+                }
+                return maxCount;
+            }
+        }
+    }
+
+    partial class Program
+    {
+        static void Task3()
+        {
+            Massive mass = new Massive(20, 0, 5);
+            Console.WriteLine("Сумма всех элементов массива: " + mass.Sum);
+            int[] inversed = mass.Inverse();
+            mass.Multi(2);
+            Console.WriteLine("Количество максимальных элементов массива: " + mass.MaxCount);
+            Console.ReadKey();
+        }
+    }
+}
